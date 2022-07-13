4719c1970b40357c1823e572d77adfad9030a497
Daniel Jansen van Vuuren <dljavavu0@gmail.com>
2022-07-13 20:59:15 +0800

    Project Code Player
    Day 1

---
+++ Javascript/JQuery/Code PLayer/ProjectCodePlayer.js
@@ -0,0 +1,65 @@
+let displayNum = 2;
+
+const adjustDisplay = function () {
+    if (displayNum === 2) {
+        $(".display").css("width", "49.93%");
+    } else if (displayNum === 3) {
+        $(".display").css("width", "33.25%");
+    } else if (displayNum === 1) {
+        $(".display").css("width", "100%");
+    } else {
+        $(".display").css("width", "24.93%");
+    };
+};
+
+$("#html-btn").click(function () {
+
+    if ($("#html-display").css("display") === "block") {
+        $("#html-display").css("display", "none");
+        displayNum--;
+    } else {
+        $("#html-display").css("display", "block");
+        displayNum++;
+    };
+
+    adjustDisplay();
+});
+
+$("#css-btn").click(function () {
+
+    if ($("#css-display").css("display") === "block") {
+        $("#css-display").css("display", "none");
+        displayNum--;
+    } else {
+        $("#css-display").css("display", "block");
+        displayNum++;
+    };
+
+    adjustDisplay();
+});
+
+$("#java-btn").click(function () {
+
+    if ($("#java-display").css("display") === "block") {
+        $("#java-display").css("display", "none");
+        displayNum--;
+    } else {
+        $("#java-display").css("display", "block");
+        displayNum++;
+    };
+
+    adjustDisplay();
+});
+
+$("#output-btn").click(function () {
+
+    if ($("#output-display").css("display") === "block") {
+        $("#output-display").css("display", "none");
+        displayNum--;
+    } else {
+        $("#output-display").css("display", "block");
+        displayNum++;
+    };
+
+    adjustDisplay();
+});
